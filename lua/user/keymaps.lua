776728be51814d8dfda8f7ef38f1b7d0e9d97848
Jose Balanza Martinez <balanza@mailbox.org>
2023-06-27 18:50:55 +0200

    Refactor keymaps and opts in neovim dotfiles

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -1,34 +1,25 @@
 local opts = { noremap = true, silent = true }
 
--- Shorten function name
 local keymap = vim.api.nvim_set_keymap
 
-local function map_keymap_to_modes(modes, lhs, rhs, options)
+local function multi_mode_keymap(modes, lhs, rhs, options)
   for _, mode in ipairs(modes) do
     keymap(mode, lhs, rhs, options)
   end
 end
 
-vim.g.mapleader = ";"
-vim.g.maplocalleader = ";"
-
 -- Nvimtree
 keymap("n", "<leader>e", ":NvimTreeToggle<cr>", opts)
 
--- Normal --
--- Better window navigation
 keymap("n", "<C-h>", "<C-w>h", opts)
 keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
 
--- greatest remap ever
-vim.keymap.set("x", "<leader>p", [["_dP]])
+keymap("x", "<leader>p", [["_dP]], opts)
 
 -- next greatest remap ever : asbjornHaland
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
-
-vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
-
+multi_mode_keymap({"n", "v"}, "<leader>y", [["+y]], opts)
+keymap("n", "<leader>Y", [["+Y]], opts)
+keymap({"n", "v"}, "<leader>d", [["_d]], opts)
 
--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,13 +1,22 @@
 local options ={
-number = true,
-expandtab = true,
-cindent = true,
-tabstop = 2,
-shiftwidth = 2,
+  number = true,
+  expandtab = true,
+  cindent = true,
+  tabstop = 2,
+  shiftwidth = 2,
+}
+
+local globals = {
+  mapleader = ";",
+  maplocalleader = ";",
 }
 
 for k, v in pairs(options) do
 	vim.opt[k] = v
+end
+
+for k, v in pairs(globals) do
+  vim.g[k] = v
 end
 
 -- vim.g.EasyMotion_startofline = 0
