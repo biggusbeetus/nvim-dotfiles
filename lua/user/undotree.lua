4aff898a901431709d409f4532b8c44c05dfc0c6
Jose Balanza Martinez <balanza@mailbox.org>
2023-06-29 21:56:13 +0200

    Add undotree options

--- lua/user/undotree.lua
+++ lua/user/undotree.lua
@@ -4,5 +4,15 @@ local M = {
   commit = "485f01efde4e22cb1ce547b9e8c9238f36566f21",
 }
 
-return M
+function M.config()
+  local options = {
+    undodir = os.getenv("HOME") .. "/.vim/undodir",
+    undofile = true,
+  }
 
+  for k, v in pairs(options) do
+    vim.opt[k] = v
+  end
+end
+
+return M
