227bf83662d2d2c7a65f02ef030d696c5adfcfc2
Jose Balanza Martinez <balanza@mailbox.org>
2023-06-29 20:16:09 +0200

    Add fugitive keymap again

--- lua/user/fugitive.lua
+++ lua/user/fugitive.lua
@@ -1,7 +1,7 @@
 local M = {
-  "tpope/vim-fugitive",
-  commit = "43f18ab9155c853a84ded560c6104e6300ad41da",
-  event = "VimEnter"
+	"tpope/vim-fugitive",
+	commit = "43f18ab9155c853a84ded560c6104e6300ad41da",
+	event = "VimEnter",
 }
 
 return M
