75a7c49667267144b4b87b294aeb145a5b36fd7a
Jose Balanza Martinez <braulio@balanza.dev>
2023-07-09 21:41:49 +0000

    Changed colorscheme to rose-pine

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,11 +1,16 @@
 local M = {
-  "rebelot/kanagawa.nvim",
-  name = "kanagawa-dragon",
+  'rose-pine/neovim',
+  name = 'rose-pine',
+  opts = {
+    variant = 'main',
+    dark_variant = 'main',
+  },
   lazy = false,    -- make sure we load this during startup if it is your main colorscheme
   priority = 1000, -- make sure to load this before all the other start plugins
 }
 
-function M.config()
+function M.config(_, opts)
+  require("rose-pine").setup(opts)
   local status_ok, _ = pcall(vim.cmd.colorscheme, M.name)
   if not status_ok then
     return
