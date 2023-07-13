fa8c289f40834ade6a11fca157cca68cb7a45a28
Jose Balanza Martinez <balanza@mailbox.org>
2023-07-13 12:36:53 +0200

    Changed from lsp-zero to just lspconfig

--- lua/user/lsp.lua
+++ lua/user/lsp.lua
@@ -1,97 +1,118 @@
 local M = {
-  "VonHeikemen/lsp-zero.nvim",
-  branch = "v2.x",
-  lazy = true,
-  event = {"BufReadPre"},
-  dependencies = {
-    -- LSP Support
-    { "neovim/nvim-lspconfig",
-      commit = "649137cbc53a044bffde36294ce3160cb18f32c7" },
-  },
+	"neovim/nvim-lspconfig",
+	commit = "649137cbc53a044bffde36294ce3160cb18f32c7",
+	lazy = false,
+	event = { "BufReadPre" },
+	dependencies = {
+		{
+			"hrsh7th/cmp-nvim-lsp",
+			commit = "0e6b2ed705ddcff9738ec4ea838141654f12eeef",
+		},
+	},
 }
 
 function M.config()
-  local lsp = require("lsp-zero")
+	local lspconfig = require("lspconfig")
+	local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
-  lsp.preset("recommended")
+	local capabilities = vim.lsp.protocol.make_client_capabilities()
+	capabilities.textDocument.completion.completionItem.snippetSupport = true
+	capabilities = cmp_nvim_lsp.default_capabilities(M.capabilities)
 
-  lsp.ensure_installed(require("lsp_dependencies").servers)
+	local sign_icons = {
+		{ name = "DiagnosticSignError", text = "E" },
+		{ name = "DiagnosticSignWarn", text = "W" },
+		{ name = "DiagnosticSignHint", text = "H" },
+		{ name = "DiagnosticSignInfo", text = "I" },
+	}
 
-  -- Fix Undefined global 'vim'
-  --lsp.nvim_workspace()
+	local function lsp_keymaps(client, bufnr)
+		local opts = { buffer = bufnr, remap = false }
 
-  local cmp = require("cmp")
-  local cmp_select = { behavior = cmp.SelectBehavior.Select }
-  local cmp_mappings = lsp.defaults.cmp_mappings({
-    ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-    ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-    ["<C-y>"] = cmp.mapping.confirm({ select = true }),
-    ["<C-Space>"] = cmp.mapping.complete(),
-  })
+		KEYMAP("n", "gd", function()
+			vim.lsp.buf.definition()
+		end, opts)
+		KEYMAP("n", "K", function()
+			vim.lsp.buf.hover()
+		end, opts)
+		KEYMAP("n", "<leader>vws", function()
+			vim.lsp.buf.workspace_symbol()
+		end, opts)
+		KEYMAP("n", "<leader>vd", function()
+			vim.diagnostic.open_float()
+		end, opts)
+		KEYMAP("n", "[d", function()
+			vim.diagnostic.goto_next()
+		end, opts)
+		KEYMAP("n", "]d", function()
+			vim.diagnostic.goto_prev()
+		end, opts)
+		KEYMAP("n", "<leader>vca", function()
+			vim.lsp.buf.code_action()
+		end, opts)
+		KEYMAP("n", "<leader>vrr", function()
+			vim.lsp.buf.references()
+		end, opts)
+		KEYMAP("n", "<leader>vrn", function()
+			vim.lsp.buf.rename()
+		end, opts)
+		KEYMAP("i", "<C-h>", function()
+			vim.lsp.buf.signature_help()
+		end, opts)
+	end
 
-  cmp_mappings["<Tab>"] = nil
-  cmp_mappings["<S-Tab>"] = nil
+	local on_attach = function(client, bufnr)
+		lsp_keymaps(bufnr)
+	end
 
-  lsp.setup_nvim_cmp({
-    mapping = cmp_mappings,
-  })
+	for _, server in pairs(require("lsp_dependencies").servers) do
+		local opts = {
+			on_attach = on_attach,
+			capabilities = capabilities,
+		}
 
-  lsp.set_preferences({
-    suggest_lsp_servers = false,
-    sign_icons = {
-      error = "E",
-      warn = "W",
-      hint = "H",
-      info = "I",
-    },
-  })
+		server = vim.split(server, "@")[1]
 
-  lsp.on_attach(function(client, bufnr)
-    local opts = { buffer = bufnr, remap = false }
+		local require_ok, conf_opts = pcall(require, "settings." .. server)
 
-    KEYMAP("n", "gd", function()
-      vim.lsp.buf.definition()
-    end, opts)
-    KEYMAP("n", "K", function()
-      vim.lsp.buf.hover()
-    end, opts)
-    KEYMAP("n", "<leader>vws", function()
-      vim.lsp.buf.workspace_symbol()
-    end, opts)
-    KEYMAP("n", "<leader>vd", function()
-      vim.diagnostic.open_float()
-    end, opts)
-    KEYMAP("n", "[d", function()
-      vim.diagnostic.goto_next()
-    end, opts)
-    KEYMAP("n", "]d", function()
-      vim.diagnostic.goto_prev()
-    end, opts)
-    KEYMAP("n", "<leader>vca", function()
-      vim.lsp.buf.code_action()
-    end, opts)
-    KEYMAP("n", "<leader>vrr", function()
-      vim.lsp.buf.references()
-    end, opts)
-    KEYMAP("n", "<leader>vrn", function()
-      vim.lsp.buf.rename()
-    end, opts)
-    KEYMAP("i", "<C-h>", function()
-      vim.lsp.buf.signature_help()
-    end, opts)
-  end)
+		if require_ok then
+			opts = vim.tbl_deep_extend("force", conf_opts, opts)
+		end
 
-  for _, server in pairs(require("lsp_dependencies").servers) do
-    local require_ok, conf_opts = pcall(require, "settings." .. server)
-    if require_ok then
-      lsp.configure(server, conf_opts)
-    end
-  end
+		lspconfig[server].setup(opts)
+	end
 
-  lsp.setup()
+	for _, sign in ipairs(sign_icons) do
+		vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
+	end
 
-  vim.diagnostic.config({
+  local config = {
     virtual_text = true,
+    signs = {
+      active = sign_icons,
+    },
+    update_in_insert = true,
+    underline = true,
+    severity_sort = true,
+    float = {
+      focusable = false,
+      style = "minimal",
+      border = "rounded",
+      source = "always",
+      header = "",
+      prefix = "",
+      suffix = "",
+    },
+  }
+
+  vim.diagnostic.config(config)
+
+  vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+    border = "rounded",
+  })
+
+  vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+    border = "rounded",
   })
 end
 
