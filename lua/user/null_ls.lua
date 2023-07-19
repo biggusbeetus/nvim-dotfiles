d6a33f300882aca5658703d8abb032deb7d37226
Jose Balanza Martinez <balanza@mailbox.org>
2023-07-19 16:03:49 +0200

    Created web-dev lsp and null_ls settings

--- lua/user/null_ls.lua
+++ lua/user/null_ls.lua
@@ -13,12 +13,9 @@ local M = {
 
 function M.config()
   local null_ls = require("null-ls")
-  -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
   local formatting = null_ls.builtins.formatting
-  -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
   local diagnostics = null_ls.builtins.diagnostics
 
-  -- https://github.com/prettier-solidity/prettier-plugin-solidity
   null_ls.setup({
     debug = false,
     sources = {
