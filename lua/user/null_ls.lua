2830584b97e0d2e648a740351d5b401febd6736b
Jose Balanza Martinez <balanza@mailbox.org>
2023-07-19 16:06:05 +0200

    Added null_ls again

--- lua/user/null_ls.lua
+++ lua/user/null_ls.lua
@@ -15,11 +15,14 @@ function M.config()
   local null_ls = require("null-ls")
   local formatting = null_ls.builtins.formatting
   local diagnostics = null_ls.builtins.diagnostics
+  local code_actions = null_ls.builtins.code_actions
 
   null_ls.setup({
     debug = false,
     sources = {
       formatting.stylua,
+      formatting.prettier,
+      code_actions.eslint,
       --			diagnostics.luacheck,
     },
   })
