b6f637fe4ec2d68de056d5bb6e73c1be8cd6e058
Jose Braulio Balanza Martinez <braulio@balanza.dev>
2025-05-20 21:01:02 +0200

    One last time really

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -45,5 +45,5 @@ KEYMAP("n", "<leader>SF", function () spectre.open_file_search({select_word=true
 
 -- leap
 require("leap")
-KEYMAP({'n', 'x', 'o'}, " ", '<Plug>(leap-anywhere)', KEYMAP_OPTS)
-KEYMAP({'n'}, "<leader> ", '<Plug>(leap)', KEYMAP_OPTS)
+KEYMAP({'n', 'x', 'o'}, "<space>", '<Plug>(leap-anywhere)', KEYMAP_OPTS)
+KEYMAP({'n'}, "<leader><space>", '<Plug>(leap)', KEYMAP_OPTS)
