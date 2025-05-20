f1abf2224a097fc7a21346f7bd829bbb1bcbdf44
Jose Braulio Balanza Martinez <braulio@balanza.dev>
2025-05-20 20:49:56 +0200

    God help

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -44,7 +44,8 @@ KEYMAP("n", "<leader>SW", function () spectre.open_visual({select_word=true}) en
 KEYMAP("n", "<leader>SF", function () spectre.open_file_search({select_word=true}) end, KEYMAP_OPTS)
 
 -- leap
-require("leap").setup()
+local leap
+leap:setup()
 KEYMAP({'n', 's'}, "<leader>S", '<Plug>(leap-anywhere)')
 KEYMAP({'n', 's'}, "<leader> ", '<Plug>(leap-anywhere)')
 KEYMAP({'x', 's',}, "<leader>S", '<Plug>(leap)')
