28ea1a06a958ee65ab7cf7f97a6539359ed68758
Jose Braulio Balanza Martinez <braulio@balanza.dev>
2025-06-13 13:42:58 +0200

    Updated treesitter

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -1,19 +1,23 @@
+local dir = os.getenv("HOME") .. "/.treesitter"
 local M = {
 	"nvim-treesitter/nvim-treesitter",
 	commit = "f976acdc9c8214145a11372d2a7ae4a032f62a39",
 	event = "VimEnter",
-	opts = {
-		parser_install_dir = "~/.treesitter"
-	}
+    opts = {
+		install_dir = dir
+    }
 }
 function M.config(_, opts)
-    vim.opt.runtimepath:prepend("~/.treesitter")
+    vim.opt.runtimepath:prepend(dir)
 	local treesitter = require("nvim-treesitter")
     treesitter.setup(opts)
     treesitter.install {
 	"lua",
 	"bash",
+    "go",
+    "python"
         }
 end
 
 return M
+
