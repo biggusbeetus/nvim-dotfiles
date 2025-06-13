b3bb370bdd0098e033b6d7ff73ca785b73c1c0d4
Jose Braulio Balanza Martinez <braulio@balanza.dev>
2025-06-13 12:03:26 +0200

    added parser directory

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -2,20 +2,16 @@ local M = {
 	"nvim-treesitter/nvim-treesitter",
 	commit = "f976acdc9c8214145a11372d2a7ae4a032f62a39",
 	event = "VimEnter",
+	opts = {
+		parser_install_dir = "~/.treesitter"
+	}
 }
-function M.config()
+function M.config(_, opts)
 	local treesitter = require("nvim-treesitter")
-    treesitter.setup()
+    treesitter.setup(opts)
     treesitter.install {
-			"lua",
-			"bash",
-            "javascript",
-            "typescript",
-            "go",
-            "markdown",
-            "markdown_inline",
-            "nix",
-            "yaml"
+	"lua",
+	"bash",
         }
 end
 
