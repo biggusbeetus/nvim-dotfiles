092759582b9050db437a1b6ccd8ed43b3f11adfa
Jose Braulio Balanza Martinez <braulio@balanza.dev>
2025-06-13 11:11:08 +0200

    Remove rainbow

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -2,13 +2,6 @@ local M = {
 	"nvim-treesitter/nvim-treesitter",
 	commit = "f976acdc9c8214145a11372d2a7ae4a032f62a39",
 	event = "VimEnter",
-	dependencies = {
-	{
-		"HiPhish/rainbow-delimiters.nvim",
-	    event = "BufReadPost",
-			commit = "55ad4fb76ab68460f700599b7449385f0c4e858e",
-		},
-	},
 }
 function M.config()
 	local treesitter = require("nvim-treesitter")
