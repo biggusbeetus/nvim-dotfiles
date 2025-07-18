228960b6afbddb688c1fdaa4707cdd4cbe5e1353
Jose Braulio Balanza Martinez <braulio@balanza.dev>
2025-07-18 19:35:45 +0200

    Remove fzf keymap or you go to jail

--- lua/user/fzf.lua
+++ lua/user/fzf.lua
@@ -14,6 +14,7 @@ function M.config(plugin, options)
   KEYMAP("n", "<leader>gx", function () fzf.quickfix() end, KEYMAP_OPTS)
   KEYMAP("n", "<leader>gB", function () fzf.buffers() end, KEYMAP_OPTS)
   KEYMAP("n", "<leader>go", function () fzf.oldfiles() end, KEYMAP_OPTS)
+  vim.keymap.del("n", "<leader>gs")
 end
 
 return M
